f9c4811cedbc342c6431f6d6dca966df70f15d8a
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2022-07-06 16:19:29 +0100

    initial work checked.

--- potter.py
+++ potter.py
@@ -11,23 +11,93 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from glob import glob
-
+from utilities import table_extraction, table_plotter, mkdir, editedNames, labels
 files = sorted(glob('S800/*.GWO'))
-
+A = [n for n in files if n.split()[2]=='A']
+B = [n for n in files if n.split()[2]=='B']
 #replaces file extensions and folder to extract case names
 names = [names.replace(".GWO", "") for names in files]
 names = [names.replace("S800/", "") for names in names]
 
 
 # %% read data from the text files
+output = 'Results/'
+output_csv = output + 'Reformatted-S800/'
+mkdir(output_csv)
+
+output_tables = output + 'Tables-S800/'
+mkdir(output_tables)
+
+
+for i in range(len(files)):
+    with open(files[i], 'r') as f:
+        lines = f.readlines()
+
+    #search for the summary table. 
+    #although format remains consistent throughout files
+    #it is better to be err on the side of caution. 
+    #each file will be searched for the data table. 
+
+    data = table_extraction(lines)
 
     
+    #column names need to be readjusted after split operation. some columns
+    #have a psace in their names
+    columns = data[0].split()
+    columns[3] = 'End Bg'
+    columns.pop(4)
+    columns[4] = 'Bl Ct'
+    columns.pop(5)
+    columns[5] = 'Com Str'
+    columns.pop(6)
+    columns[6] = 'Ten Str'
+    columns.pop(7)
+    
+    
+        
+    units = data[1].split()
+    #initialize array depending on size of input data
+    #13 columns remain fixed. number of rows will change. 
+    rows = np.zeros((len(data)-1, len(columns)), dtype=object)
+    rows[0, :] = units 
 
 
+#go line by line and extract the values in each row after using split()
+
+    for j in range(2, len(data)):
+        if len(data[j].split()) == 8:
+            s = data[j].split()[5] #extreact the 5th element with the joint string
+            index = s.find('-') #identify index of minus
+            x = s[:index] #extract value of left of -
+            y = s[index:] #extract value right of minus with a - sign
+            row_val = (data[j].split()) #before filling in the row, create a new variable to adjust and shift valuees
+            row_val[5] = x
+            row_val.insert(6, y)
+            rows[j-1, :] = row_val
+            
+        else: 
+            rows[j-1, :] = data[j].split()
+
+
+#write files to txt
+    df = pd.DataFrame(data = rows, columns = columns)
+    df.to_csv(output_csv + names[i] + '.txt', sep='\t', mode='a', index=False)
+
+    
+#generate tables for 
+    path = output_tables + names[i] + '/'
+    table_plotter(df, path)
     
     
+# =============================================================================
+#     
+# =============================================================================
+# #   tester below  
+# =============================================================================
+# =============================================================================
     
-with open(files[0], 'r') as f:
+# %% tester
+with open(files[1], 'r') as f:
     lines = f.readlines()
 
 
@@ -35,11 +105,15 @@ with open(files[0], 'r') as f:
 #although format remains consistent throughout files
 #it is better to be err on the side of caution. 
 #each file will be searched for the data table. 
-from utilities import table_extraction
+
 data = table_extraction(lines)
-
-
-#column names need to be readjusted after split operation. some columns
+search = 'Total'
+for i, l in enumerate(lines):
+    if len(l.split()) != 0:
+        dummy = l.split()
+        if dummy[0] == search:
+            print(i)
+            #column names need to be readjusted after split operation. some columns
 #have a psace in their names
 columns = data[0].split()
 columns[3] = 'End Bg'
@@ -85,6 +159,54 @@ pd.set_option('display.width', -1)
 pd.set_option('display.max_rows', None)
 import dataframe_image as dfi
 dfi.export(df.iloc[:40], "table.png")
-dfi.export(df.iloc[40])
 
+
+
+
+#ensure a directory exisits for this particular file to dump the images in
+path = output_tables + names[1] + '/'
+mkdir(path)
+
+
+pd.set_option("display.max_column", None)
+pd.set_option("display.max_colwidth", None)
+pd.set_option('display.width', -1)
+pd.set_option('display.max_rows', None)
+if len(df) / 40 > 1:
+    times = int(len(df) / 40) + 1 #determines number of images the df has to be broken down into
+                             #+1 take cares of the leftover values
+    
+    for i in range((times)):
+        dfi.export(df.iloc[(40*i):(40 + 40 * i)], path  + '{}.png'.format(i))
+else:
+    dfi.export(df, output_tables + names[i] + '.png')
 # %% graphs
+#create relational lists. map filenames, to gra and grb
+#first do it for A
+
+gra, grb = editedNames(names)
+gr = gra + grb
+lab = labels(gr)  #reformat labels to the desired format
+#create relational dictionary
+dict(zip(files, lab)) 
+
+df2 = df.set_index('Depth')
+df2 = df2.iloc[1:]
+df2['Bl Ct'] = df2['Bl Ct'].astype(float) / 4 #convert datatype frm string to float. divide by 4 to get /25cm value
+
+fig, ax = plt.subplots(figsize = (5, 20))
+ax.plot(df2['Bl Ct'], df2.index, linewidth = 1, alpha = 0.5, label = names[1])
+
+ax.set_xlabel(r'Blow Count (Blows/25cm)')
+ax.set_ylabel(r'Depth (m)')
+x = np.arange(0, 100, 20) # define the x to make ti the same for all cpts
+y = np.arange(0, 100, 5)
+ax.set_xticks(x)
+ax.set_yticks(y)
+ax.invert_yaxis()
+ax.xaxis.set_label_position('top')
+ax.xaxis.tick_top()
+ax.grid()
+ax.legend(loc='upper center',
+        ncol=6)
+plt.show()
--- utilities.py
+++ utilities.py
@@ -5,7 +5,9 @@ Created on Tue Jul  5 12:07:05 2022
 
 @author: goharshoukat
 """
-
+import os
+import dataframe_image as dfi
+import pandas as pd
 def list_search(data, search):
     #function to search and return the index for which a keyword is seen in the file. 
     
@@ -21,6 +23,104 @@ def table_extraction(data):
     #Output : pd.DataFrame : data table bounded between SUMMARY and Total. 
     
     i = list_search(data, 'SUMMARY')
-    j = list_search(data, 'Total')
+    try:
+        j = list_search(data, 'Total')
+        
+        return data[(i+3):(j-1)]
+    except:
+        j = list_search(data, 'Refusal')
+        return data[(i+3):(j-1)]
     
-    return data[i+3:j-1]
+def mkdir(path):
+    #Input
+    #path : str : path to make a directory
+    
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    
+    return True
+
+
+def table_plotter(df, filename):
+    #Input
+    #df : dataframe : dataframe with the information
+    #divides it into rows of 40 and puts it in a folder with the name of the file
+    #as the directory name
+    #divide the rowns into sequences of 40
+    #filename : str : has to have a parent directory mentioned in the name
+    
+    
+    #ensure a directory exisits for this particular file to dump the images in
+    path = filename
+    mkdir(path)
+    
+    
+    pd.set_option("display.max_column", None)
+    pd.set_option("display.max_colwidth", None)
+    pd.set_option('display.width', -1)
+    pd.set_option('display.max_rows', None)
+    if len(df) / 40 > 1:
+        times = int(len(df) / 40) + 1 #determines number of images the df has to be broken down into
+                                 #+1 take cares of the leftover values
+        
+        for i in range((times)):
+            dfi.export(df.iloc[(40*i):(40 + 40 * i)], path  + '{}.png'.format(i))
+    else:
+        dfi.export(df, filename + '.png')
+
+
+def editedNames(names):
+    #Input
+    #names : list of list : takes the list of names
+    #edits them to make the labels like the zawtika file
+    
+    gra = []
+    grb = []
+    j = 0
+    for i, n in enumerate(names):
+        n = n.replace('14 GR', '')
+        if n.split()[0] == 'A':
+            
+            gra.append(n)
+            
+            gra[i] = gra[i].split()
+            
+    
+            #change 'S-xxx' to 'xxxS'
+            gra[i][-2] = gra[i][-2].replace('S-', "")
+            gra[i][-2] = gra[i][-2].replace(gra[i][-2], gra[i][-2] + 'S')
+            
+            #change E=xx to xx%
+            gra[i][-1] = gra[i][-1].replace('E=', "")
+            gra[i][-1] = gra[i][-1].replace(gra[i][-1], gra[i][-1] + '%')
+    
+            
+            
+        #create a seperate group for group b results
+        elif n.split()[0] == 'B':
+            grb.append(n)
+            grb[j] = grb[j].split()
+            
+    
+            #change 'S-xxx' to 'xxxS'
+            grb[j][-2] = grb[j][-2].replace('S-', "")
+            grb[j][-2] = grb[j][-2].replace(grb[j][-2], grb[j][-2] + 'S')
+            
+            #change E=xx to xx%
+            grb[j][-1] = grb[j][-1].replace('E=', "")
+            grb[j][-1] = grb[j][-1].replace(grb[j][-1], grb[j][-1] + '%')
+            
+            j = j + 1
+        
+    return gra, grb
+
+def labels(lst):
+    #Input
+    #takes output from the editedNames function above and converts them
+    #into labels
+    lab = []
+    for l in lst:
+        lab.append('{} {} {}'.format(l[-2], l[-3], l[-1]))
+        
+    return lab
+    
